9367d2af1c1a3860b7469a921dea1a415b928da8
gulpiegator <tristin.moore@rocketmail.com>
2024-04-02 16:22:58 -0400

    created a decent mockup of the prebuilt page that should translate well to using a database in the future

--- app.py
+++ app.py
@@ -14,13 +14,36 @@ def showStartBuild():
 def showFAQ():
     return render_template('faq.html')
 
-@app.get('/preBuilts')
+@app.route('/preBuilts')
 def showPreBuilts():
-    return render_template('preBuilts.html')
-
-@app.get('/browse')
-def showBrowse():
-    return render_template('browse.html')
+    content = request.args.get('content', 'gaming')
+    image_url = 'https://ralfvanveen.com/wp-content/uploads//2021/06/Placeholder-_-Begrippenlijst.svg'
+    if content == 'gaming':
+        data = [{'name': 'pc1', 'price': '145.00', 'rating': 4.8, 'image_url': image_url},
+                {'name': 'pc2', 'price': '270.00', 'rating': 3.4, 'image_url': image_url},
+                {'name': 'pc3', 'price': '500.50', 'rating': 2.1, 'image_url': image_url},
+                {'name': 'pc4', 'price': '200.00', 'rating': 4.5, 'image_url': image_url},
+                {'name': 'pc5', 'price': '310.00', 'rating': 3.6, 'image_url': image_url},
+                {'name': 'pc6', 'price': '480.00', 'rating': 3.3, 'image_url': image_url},
+                {'name': 'pc7', 'price': '180.00', 'rating': 4.2, 'image_url': image_url},
+                {'name': 'pc8', 'price': '320.00', 'rating': 3.9, 'image_url': image_url},
+                {'name': 'pc9', 'price': '450.00', 'rating': 3.1, 'image_url': image_url},
+                {'name': 'pc10', 'price': '160.00', 'rating': 4.0, 'image_url': image_url},
+                {'name': 'pc11', 'price': '290.00', 'rating': 3.7, 'image_url': image_url},
+                {'name': 'pc12', 'price': '550.00', 'rating': 2.9, 'image_url': image_url}]
+    elif content == 'recording':
+        data = [{'name':'pc1', 'price':'180.00', 'rating':4.2, 'image_url': image_url},
+                {'name':'pc2', 'price':'320.00', 'rating':3.9, 'image_url': image_url},
+                {'name':'pc3', 'price':'450.00', 'rating':3.1, 'image_url': image_url}]
+    elif content == 'office':
+        data = [{'name':'pc1', 'price':'200.00', 'rating':4.5, 'image_url': image_url},
+                {'name':'pc2', 'price':'290.00', 'rating':3.7, 'image_url': image_url},
+                {'name':'pc3', 'price':'550.00', 'rating':2.9, 'image_url': image_url}]
+    elif content == 'school':
+        data = [{'name':'pc1', 'price':'160.00', 'rating':4.0, 'image_url': image_url},
+                {'name':'pc2', 'price':'310.00', 'rating':3.6, 'image_url': image_url},
+                {'name':'pc3', 'price':'480.00', 'rating':3.3, 'image_url': image_url}]
+    return render_template('preBuilts.html', data=data)
 
 @app.get('/cart')
 def showCart():
