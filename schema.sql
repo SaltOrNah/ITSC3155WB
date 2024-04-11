9601b05a5734a24efa47e601d9d124bf65dc31ed
Ben <norwegiannarwhaler@gmail.com>
2024-04-10 21:51:24 -0400

    removde email from schema

--- schema.sql
+++ schema.sql
@@ -5,7 +5,6 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users(
     user_id         SERIAL          NOT NULL,
     username        VARCHAR(255)    UNIQUE NOT NULL,
-    email           VARCHAR(255)    UNIQUE NOT NULL,
     hashed_password VARCHAR(255)    NOT NULL,
     is_admin        BOOLEAN         NOT NULL,
     PRIMARY KEY (user_id)
